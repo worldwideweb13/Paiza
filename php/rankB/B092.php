ead4c847d17b2283cc4bea6bac9fcdccc1a771c4
ogasawarasatoru <world.wide.web.13@gmail.com>
2022-02-04 04:13:43 +0900

    B092:【キャンペーン問題】セーブポイント

---
+++ php/rankB/B092.php
@@ -0,0 +1,57 @@
+<?php
+    /*　回答方針 
+    * (1) 現在地とセーブポイントの座標(x,y)を取得して、それぞれ変数、配列化
+    * (2) セーブポイント座標配列を昇順【ksort()】にして、現在地座標との距離を算出
+    * (3) (2)の結果配列から最小値を取得【min()】,最小値を持つ座標一覧を取得【array_keys()】
+    * (4) (3)を画面に表示
+    */
+    
+    // (1) 現在地とセーブポイントの座標(x,y)を取得して、それぞれ変数、配列化
+    [$H,$W,$K] = explode(" ", trim(fgets(STDIN)));
+    $N = [];
+    $pointsArr = [];
+    for ($i = 0; $i_y < $H; $i_y++) {
+         $rowArr = str_split(trim(fgets(STDIN)));
+         foreach ($rowArr as $i_x => $val) {
+             if($val == "N"){
+                 $N["x"] = $i_x + 1;
+                 $N["y"] = $i_y + 1;
+             }elseif ($val == "#") {
+                 continue;
+             }else{
+                $pointsArr[$val]["x"] = $i_x + 1;
+                $pointsArr[$val]["y"] = $i_y + 1;
+             }
+         }
+    }
+    
+    // (2) セーブポイント座標配列を昇順【ksort()】にして、現在地座標との距離を算出
+    $distanceArr = calDistance($N,$pointsArr);
+    
+    function calDistance($N,$pointsArr){
+        $distanceArr = [];
+        foreach ($pointsArr as $key => $point) {
+            $distance = abs($N["x"] - $point["x"]) + abs($N["y"] - $point["y"]);
+            $distanceArr[$key] = $distance;
+        }
+        return $distanceArr;
+    }
+    
+    // (3) (2)の結果配列から最小値を取得【min()】,最小値を持つ座標一覧を取得【array_keys()】
+    $minDistanceArr = minDistance($distanceArr);
+    
+    function minDistance($distanceArr){
+        $minDistanceArr = [];
+        $minKeys = array_keys($distanceArr,min($distanceArr));
+        foreach ($minKeys as $key) {
+            array_push($minDistanceArr,$key);
+        }
+        //  (4) (3)を画面に表示
+        asort($minDistanceArr);
+        echo count($minDistanceArr).PHP_EOL;
+        foreach ($minDistanceArr as $val) {
+            echo $val.PHP_EOL;
+        }        
+    }
+    
+?>
