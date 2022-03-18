114d926174dacccbabf3970c83adde1f718c31d8
ogasawarasatoru <world.wide.web.13@gmail.com>
2022-03-19 02:32:44 +0900

    B082:休暇の取り方

--- php/rankB/B082.php
+++ php/rankB/B082.php
@@ -8,7 +8,6 @@
     * (3) $countArrの中で最も大きい数字を出力
     */
     
-    
     // (1) N,M,workの数(w_M)を変数化,日程を配列化...scheduleArr
     [$N,$M] = array_map('intval', explode(" ", trim(fgets(STDIN))));
     for ($i = 0; $i < $N; $i++) {
@@ -23,11 +22,13 @@
     for ($i = 0; $i < $count; $i++) {
         // (2-1) scheduleArrの最初の"work"から"off"にM回連続して入れ変える
          [$editArr,$index] = editArr($scheduleArr,$M,$w_M,$index);
-        //  $ans = countArr();
+         $ans = countArr($editArr);
+        //  var_dump($editArr);
+        //  var_dump($ans);
+        $ansArr[] = $ans;
     }
     
-    
-    
+    echo max($ansArr).PHP_EOL;
     
     function editArr($scheduleArr,$M,$w_M,$index){
         // 働く日のkey配列を取得
@@ -43,18 +44,23 @@
             return [$scheduleArr,$index];
     }
     
-
-    for ($i = 0; $i < $N; $i++) {
-        if(!$scheduleArr[$i])
-          $scheduleArr[$i] = "追加";
+    function countArr($editArr){
+        $ans = 0;
+        $count = 0;
+        $arr_last_key = array_key_last($editArr);
+        foreach ($editArr as $i => $shift) {
+            if ($shift === "off") {
+                $count++;
+                // 配列の最後の時は前回のoffの連続回数と比較
+                if($i === $arr_last_key && $ans <= $count){
+                    $ans = $count;
+                }
+            }else{
+                $ans = $ans <= $count ? $count : $ans;
+                $count = 0;
+            }
+        }
+        return $ans;
     }
-    
-    // ksort($scheduleArr);
-    // print($arr);
-    // foreach ($scheduleArr as $key => $val) {
-    //     echo "$key = $val\n";
-    // }
-    
-
     
 ?>
